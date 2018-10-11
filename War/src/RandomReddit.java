db88836e345f085cd360afe58ede2dc15d0f5b77
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-10-11 14:44:28 -0400

    small changes

--- War/src/RandomReddit.java
+++ War/src/RandomReddit.java
@@ -1,18 +1,24 @@
 import java.io.IOException;
-
+import java.util.Scanner;
 
 /**
  * 
- * @author trossos
- * This program opens 5 reddit/r/random pages
+ * @author trossos This program opens 5 reddit/r/random pages
  */
 
 public class RandomReddit {
+	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
-
-		for (int i = 0; i < 6; i++) {
-			java.awt.Desktop.getDesktop().browse(java.net.URI.create("www.reddit.com/r/random"));
+		while (true) {
+			System.out.println("enter any character to open 5 random subreddits");
+			String temp = in.next();
+			if (!temp.equals("")) {
+				for (int i = 0; i < 6; i++) {
+					java.awt.Desktop.getDesktop().browse(java.net.URI.create("www.reddit.com/r/random"));
+				}
+				temp = "";
+			}
 		}
 	}
 }
